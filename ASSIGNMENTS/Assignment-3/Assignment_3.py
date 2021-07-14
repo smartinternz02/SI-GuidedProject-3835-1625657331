9a147b3e37b4ffa3a87bde39be456d06c7e8cac6
vighnesh1209 <vighnesh1209@gmail.com>
2021-07-14 23:43:37 +0530

    Update Assignment_3.py

--- ASSIGNMENTS/Assignment-3/Assignment_3.py
+++ ASSIGNMENTS/Assignment-3/Assignment_3.py
@@ -15,15 +15,18 @@ myConfig = {
 def myCommandCallback(cmd):
     print("Message received from IBM IoT Platform: %s" % cmd.data['command'])
     
-client = wiotp.sdk.device.DeviceClient(config=myConfig, logHandlers=None)
-client.connect()
 
-while True:
-    temp=random.randint(0,100)
-    hum=random.randint(0,100)
-    myData={'waterlevel':temp, 'intensity':hum}
-    client.publishEvent(eventId="status", msgFormat="json", data=myData, qos=0, onPublish=None)
-    print("Published data Successfully: %s", myData)
-    client.commandCallback = myCommandCallback
-    time.sleep(2)
-client.disconnect()
+try:
+    client = wiotp.sdk.device.DeviceClient(config=myConfig, logHandlers=None)
+    client.connect()
+
+    while True:
+        inten=random.randint(0,100)
+        water=random.randint(0,100)
+        myData={'waterlevel':water, 'intensity':inten}
+        client.publishEvent(eventId="status", msgFormat="json", data=myData, qos=0, onPublish=None)
+        print("Published data Successfully: %s", myData)
+        client.commandCallback = myCommandCallback
+        time.sleep(2)
+except KeyboardInterrupt:
+    client.disconnect()

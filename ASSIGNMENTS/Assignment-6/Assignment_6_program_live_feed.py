c380aa0fa4eaa91496e346e7705b76797837c26a
vighnesh1209 <vighnesh1209@gmail.com>
2021-07-17 23:24:29 +0530

    Update Assignment_6_program_live_feed.py

--- ASSIGNMENTS/Assignment-6/Assignment_6_program_live_feed.py
+++ ASSIGNMENTS/Assignment-6/Assignment_6_program_live_feed.py
@@ -9,6 +9,10 @@ smile_classifier=cv2.CascadeClassifier("haarcascade_smile.xml")
 #It will read the first frame/image of the video
 video=cv2.VideoCapture(0)
 
+frame_width = int(video.get(3))
+frame_height = int(video.get(4))
+out = cv2.VideoWriter('output_FACE.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
+
 while True:
     #capture the first frame
     check,frame=video.read()
@@ -25,6 +29,7 @@ while True:
     for(x,y,w,h) in faces:
         cv2.rectangle(frame, (x,y), (x+w,y+h), (255,0,0), 2)
         cv2.imshow('Face detection', frame)
+        out.write(frame)
         #picname=datetime.datetime.now().strftime("%y-%m-%d-%H-%M")
         #cv2.imwrite(picname+".jpg",frame)
         
@@ -32,19 +37,23 @@ while True:
     for(ex,ey,ew,eh) in eyes:
         cv2.circle(frame, (ex+ew//2,ey+eh//2), ew//2 , (0,255,0), 3)
         cv2.imshow('Face detection', frame)
+        out.write(frame)
     
     for(sx,sy,sw,sh) in smile:
         cv2.rectangle(frame, (sx,sy), (sx+sw,sy+sh), (0,0,255), 2)
         cv2.imshow('Face detection', frame)
+        out.write(frame)
     for(sx,sy,sw,sh) in smile:
         cv2.rectangle(frame, (sx,sy), (sx+sw,sy+sh), (255,255,0), 2)
         cv2.imshow('Face detection', frame)
+        out.write(frame)
     
     #waitKey(1)- for every 1 millisecond new frame will be captured
     Key=cv2.waitKey(1)
     if Key==ord('q'):
         #release the camera
         video.release()
+        out.release()
         #destroy all windows
         cv2.destroyAllWindows()
         break
